9b47c2a39795f35a447fc820afe61244de4cfe55
kavitappatil <patil.poem@gmail.com>
2023-05-17 23:52:52 +0100

    first commit

---
+++ index.test.js
@@ -0,0 +1,27 @@
+// it("should work", () => {
+//   const left = 1;
+//   const right = 2;
+
+//   const result = left + right;
+
+//   expect(result).toBe(3);
+// });
+// it("should work", () => {
+//   const left = 2;
+//   const right = 2;
+
+//   const result = left + right;
+
+//   expect(result).toBe(5);
+// });
+
+describe("rock, paper, scissors", () => {
+  it("should say left wins for rock vs. scissors", () => {
+    const left = "rock";
+    const right = "scissors";
+
+    const outcome = rps(left, right);
+
+    expect(outcome).toBe("left");
+  });
+});
